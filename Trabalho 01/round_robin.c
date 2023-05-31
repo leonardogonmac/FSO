5ba7b64f4021319755a4ef270382755b3d0ee1f9
Leonardo Gonçalves Machado <89925347+leonardogonmac@users.noreply.github.com>
2023-05-31 17:04:10 -0300

    round robin atualizado

--- Trabalho 01/round_robin.c
+++ Trabalho 01/round_robin.c
@@ -4,38 +4,68 @@
 typedef struct process{
     int pid;
     int exec;
-    int end;
+    struct process* next;
+    struct process* previous;
 
 }process;
 
+typedef struct head{
+    process* first;
+    process* last;
+    int flag;
+}head;
+
+void enfia(head* h, int pid, int exec){
+    process* p = malloc(sizeof(process));
+    p->pid = pid;
+    p->exec = exec*1000;
+    p->next = NULL;
+    if(h->flag == 0){
+        h->first = p;
+        h->last = p;
+        h->flag++;
+    }
+    else{
+        h->last->next = p;
+        p->previous = h->last;
+        h->last = p;
+    }
+}
+
+
 int main(){
-    int n, t, pid, exec, clock = 0, total = 0, clock_aux = 0;
+    head h;
+    h.flag = 0;
+    h.first = NULL;
+    h.last = NULL;
+    int n, quantum, pid, exec, clock = 0, total = 0, clock_aux = 0;
     scanf("%d", &n);
-    scanf("%d", &t);
-    process* v = malloc(sizeof(process)*n);
+    scanf("%d", &quantum);
 
     for(int i = 0; i < n; i++){
         scanf("%d %d", &pid, &exec);
-        v[i].pid = pid;
-        v[i].exec = exec*1000;
-        total += v[i].exec;
+        enfia(&h, pid, exec);
+        total += exec*1000;
     }
 
-    int swich = 0;
+    h.last->next = h.first;
+    h.first->previous = h.last;
+
+    process* current = h.first;
     while(clock < total){
-        while(clock_aux < t && v[swich].exec > 0){
+        while(clock_aux < quantum){
             clock++;
-            clock_aux++; 
-            v[swich].exec--;
-            if(v[swich].exec == 0) {
-                v[swich].end = clock;
-                printf("%d (%d)\n", v[swich].pid, v[swich].end);
+            clock_aux++;
+            current->exec--;
+            if(current->exec == 0){
+                printf("%d (%d)\n", current->pid, clock);
+                current->previous->next = current->next;
+                current->next->previous = current->previous;
                 break;
             }
         }
-        swich = (swich + 1) % n;
+        current = current->next;
         clock_aux = 0;
-
     }
 
     return 0;
